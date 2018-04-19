67d63ca21e104a257e010514d63782a0e8191e3d
AlexLaak <alex.laakkonen@metropolia.fi>
2018-04-19 11:19:04 +0300

    testing

--- src/recipe/RecipeDB.java
+++ src/recipe/RecipeDB.java
@@ -7,6 +7,7 @@ package recipe;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -23,20 +24,31 @@ public class RecipeDB {
         
         Statement stmt = connection.createStatement();
         
-        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        //stmt.executeUpdate("DROP TABLE IF EXISTS test");
+        //stmt.executeUpdate("CREATE TABLE test (name varchar(255),pena varchar(255))"); 
+        stmt.executeUpdate("INSERT INTO test VALUES ('testing','again')");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM test");
         
+        
+        
+        //For printing 'Show tables'
+        /*
+        ResultSet rs = stmt.executeQuery("SELECT table_name\n" +
+                "  FROM information_schema.tables\n" +
+                " WHERE table_schema='public'\n" +
+                "   AND table_type='BASE TABLE';");
+        
+        */
         while (rs.next()) {
-            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+            System.out.println(rs.getString(1));
+            //System.out.println("Read from DB: " + rs.getTimestamp("tick"));
         }
        
     }
     
+    //Connection to database. Dont modify this
     private static Connection getConnection() throws URISyntaxException, SQLException {
         URI dbUri = new URI("postgres://hkullkvwogqmsk:57f03e071224c52d64f523e55c0105096f73c5e1fc5519ac6e4af2461419ebdd@ec2-54-217-217-142.eu-west-1.compute.amazonaws.com:5432/d1ur8rbqa8ddlp");
-        System.out.println("HALOO");
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         System.out.println(dbUri.getHost());
