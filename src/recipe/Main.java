2747b6b52e6b4b55b8c4b8768fc2b930e08f5a9a
AlexLaak <alex.laakkonen@metropolia.fi>
2018-04-24 12:34:40 +0300

    remove broken gradle files

--- src/recipe/Main.java
+++ src/recipe/Main.java
@@ -1,9 +1,11 @@
+package recipe;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package recipe;
+
 
 import java.net.URI;
 import java.net.URISyntaxException;
