0631ffea19ee5b2780cbca4a8040892552e1861d
Khoa Pham <kenjifam@gmail.com>
2018-05-07 02:54:46 +0300

    added "edit recipe"

--- src/main/java/recipe/UI/FormController.java
+++ src/main/java/recipe/UI/FormController.java
@@ -14,6 +14,7 @@ import java.sql.Statement;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import main.java.recipe.Recipe;
 
 /**
  * FXML Controller class
@@ -47,7 +48,27 @@ public class FormController {
         connection.close();
     }
 
-    
+    public void editRecipe(Recipe rcp) {
+        nameField.setText(rcp.getName());
+        ingredientsField.setText(rcp.getIngredients());
+        instructionsField.setText(rcp.getInstructions());
+        tagsField.setText(rcp.getTags());
+    }
+
+    public Recipe updateRecipe(Recipe rcp) throws URISyntaxException, SQLException {
+        rcp.setName(nameField.getText());
+        rcp.setIngredients(ingredientsField.getText());
+        rcp.setInstructions(instructionsField.getText());
+        rcp.setTags(tagsField.getText());
+
+        Connection connection = getConnection();
+        Statement stm = connection.createStatement();
+        stm.executeUpdate("UPDATE recipes SET name = '" + rcp.getName() + "', ingredients = '" + rcp.getIngredients() + "', instructions = '" 
+                + rcp.getInstructions() + "', tags = '" + rcp.getTags() + "' WHERE id = '" + rcp.getId() + "'");
+        connection.close();
+        
+        return rcp;
+    }
     
     private static Connection getConnection() throws URISyntaxException, SQLException {
         URI dbUri = new URI("postgres://hkullkvwogqmsk:57f03e071224c52d64f523e55c0105096f73c5e1fc5519ac6e4af2461419ebdd@ec2-54-217-217-142.eu-west-1.compute.amazonaws.com:5432/d1ur8rbqa8ddlp");
--- src/main/java/recipe/UI/RecipelistController.java
+++ src/main/java/recipe/UI/RecipelistController.java
@@ -9,13 +9,19 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -31,7 +37,16 @@ import main.java.recipe.RecipeList;
 public class RecipelistController {
     
     @FXML
+    private AnchorPane mainPane;
+    
+    @FXML
     private VBox boxRecipe;
+    
+    @FXML
+    private Button btn_edit;
+    
+    @FXML
+    private Button btn_delete;
     
     @FXML
     private Label idLabel;
@@ -64,9 +79,6 @@ public class RecipelistController {
     private ImageView recipeImage;
     
     @FXML
-    private AnchorPane firstAnchor;
-    
-    @FXML
     private AnchorPane secondAnchor;
     
     private RecipeList recipelist;
@@ -81,12 +93,14 @@ public class RecipelistController {
      */
     public void initialize() throws ClassNotFoundException, SQLException, URISyntaxException {
         recipelist = new RecipeList();
-        recipeArray = Controller.getRecipeArray();
+        recipeArray = RecipeList.getAllRecipes();
+        recipeArray.sort((Recipe o1, Recipe o2) -> o1.getId() - o2.getId());  // After update the recipe go to the end of the list in database -> need sort
         showList();
         showRecipe(recipeArray.get(recipeArray.size() - 1));
     }
     
     public void showList() {
+        boxRecipe.getChildren().clear();
         recipeArray.forEach(rcp -> {
             VBox rcpBox = new VBox();
             Text rcpTitle = new Text("Recipe #" + rcp.getId() + " " + rcp.getName());
@@ -109,6 +123,13 @@ public class RecipelistController {
         ingredients.setText(rcp.getIngredients());
         instructions.setText(rcp.getInstructions());
         recipeImage.setImage(new Image(MainUI.class.getResourceAsStream("recipe.jpg")));
+        btn_edit.setOnAction(event -> {
+            try {
+                showEditRecipeDialog(rcp);
+            } catch (URISyntaxException | SQLException | ClassNotFoundException ex) {
+                Logger.getLogger(RecipelistController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
     }
     
     public void showAddRecipeForm() throws IOException {
@@ -117,5 +138,43 @@ public class RecipelistController {
         secondAnchor.getChildren().add(listPane); 
     }
     
+    public void showEditRecipeDialog(Recipe rcp) throws URISyntaxException, SQLException, ClassNotFoundException {
+        if(rcp == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No Recipe Selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select the recipe you want to edit.");
+            alert.showAndWait();
+            return;
+        }
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainPane.getScene().getWindow());
+        dialog.setTitle("Edit Recipe");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("form.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        FormController formController = fxmlLoader.getController();
+        formController.editRecipe(rcp);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            rcp = formController.updateRecipe(rcp);
+            recipeArray = RecipeList.getAllRecipes();
+            recipeArray.sort((Recipe o1, Recipe o2) -> o1.getId() - o2.getId());
+            showList();
+            showRecipe(rcp);
+        }
+    }
     
 }
